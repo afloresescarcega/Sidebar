f64f85c3ae92d3868e221b3373f7b0ac666b23ec
Alex Flores Escarcega <alexfloresescarcega@wireless-10-147-137-47.public.utexas.edu>
2020-02-17 15:15:06 -0600

    Basic api is working

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,6 +1,6 @@
 #!flask/bin/python
 from flask import Flask, jsonify, Response
-from . import Reddit_access
+from reddit_access import Reddit_access
 
 app = Flask(__name__)
 
@@ -14,6 +14,12 @@ Get Reddit token
 """
 import requests
 import requests.auth
+from pprint import pprint
+import json
+import re
+from flask import request
+
+
 
 REDDIT_CLIENT_ID = None
 REDDIT_SECRET = None
@@ -23,12 +29,12 @@ REDDIT_USERNAME_PASSWORD = None
 
 
 @app.route('/sidebar/api/v1.0/tasks', methods=['GET'])
-def get_sidebar_subreddits(subreddit):
+def get_sidebar_subreddits():
     """
     Receives a single string of the subreddit.
     Returns a list of subreddits that were mentioned in the sidebar of the subreddit
     """
-
+    subreddit = request.args.get('subreddit', default = '*', type = str)
     """
     Bad subreddit name input
     """
@@ -37,9 +43,23 @@ def get_sidebar_subreddits(subreddit):
     if len(subreddit) > MAX_SUBREDDIT_LEN:
         return Response("{None}", status=403, mimetype='application/json')
 
+    headers = {"User-Agent": "Sidebar by heliopphobicdude"}
     # Make a call to reddit asking for the sidebar of that subreddit
-    
-    return jsonify({'subreddits': subreddits})
+    r = requests.get('https://www.reddit.com/r/'+subreddit+'/about.json', headers=headers)
+    data = r.json()
+
+    print(data.keys())
+    print("This is data")
+
+    sidebar_raw = ""
+    for k, v in data.items():
+        if k == 'data':
+            sidebar_raw = v['description']
+            print("Heheheh", sidebar_raw)
+    sidebars_list = [match.strip() for match in re.findall("\/r\/([^\/&()\s]*)", sidebar_raw)]
+    print("This is the output of the regular expressions", sidebars_list)
+    sidebars = set(sidebars_list)
+    return jsonify({'subreddits': list(sidebars)})
 
 if __name__ == '__main__':
     token_handler = Reddit_access()
