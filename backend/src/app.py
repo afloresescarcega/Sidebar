1643b537b8a5a74f652a317a831efb11f17aa7f9
Alex Flores Escarcega <alexfloresescarcega@Alexs-MacBook-Pro.local>
2020-02-19 22:45:08 -0600

    Made frontend with nodes and text

--- backend/src/app.py
+++ backend/src/app.py
@@ -18,6 +18,7 @@ from pprint import pprint
 import json
 import re
 from flask import request
+import os
 
 
 
@@ -26,8 +27,11 @@ REDDIT_SECRET = None
 REDDIT_USERNAME = None
 REDDIT_USERNAME_PASSWORD = None
 
-@app.route("/")
+@app.route("/sidebar")
 def index():
+    path = os.getcwd()
+
+    print(path)
     return render_template("index.html", message="Hello Flask!");
 
 
@@ -37,18 +41,18 @@ def get_sidebar_subreddits():
     Receives a single string of the subreddit.
     Returns a list of subreddits that were mentioned in the sidebar of the subreddit
     """
-    subreddit = request.args.get('subreddit', default = '*', type = str)
+    SUBREDDIT = request.args.get('subreddit', default = '*', type = str)
     """
     Bad subreddit name input
     """
-    if not subreddit:
+    if not SUBREDDIT:
         return Response("{None}", status=403, mimetype='application/json')
-    if len(subreddit) > MAX_SUBREDDIT_LEN:
+    if len(SUBREDDIT) > MAX_SUBREDDIT_LEN:
         return Response("{None}", status=403, mimetype='application/json')
 
     headers = {"User-Agent": "Sidebar by heliopphobicdude"}
     # Make a call to reddit asking for the sidebar of that subreddit
-    r = requests.get('https://www.reddit.com/r/'+subreddit+'/about.json', headers=headers)
+    r = requests.get('https://www.reddit.com/r/'+SUBREDDIT+'/about.json', headers=headers)
     data = r.json()
 
     print(data.keys())
@@ -59,10 +63,32 @@ def get_sidebar_subreddits():
         if k == 'data':
             sidebar_raw = v['description']
             print("Heheheh", sidebar_raw)
-    sidebars_list = [match.strip() for match in re.findall("\/r\/([A-Za-z0-9][A-Za-z0-9_]{2,20})", sidebar_raw)]
+    sidebars_list = [match.strip().lower() for match in re.findall("\/r\/([A-Za-z0-9][A-Za-z0-9_]{2,20})", sidebar_raw)]
     print("This is the output of the regular expressions", sidebars_list)
-    sidebars = set(sidebars_list)
-    return jsonify({'subreddits': list(sidebars)})
+    sidebars = list(set(sidebars_list))
+
+    # Create the graph
+    nodes = []
+    links = []
+    query_subreddit_id = -1
+
+    # Nodes
+    for i, subreddit_name in enumerate(sidebars):
+        if subreddit_name != SUBREDDIT:
+            node = {"id": i, "name": subreddit_name}
+            nodes.append(node)
+
+    node = {"id": -1, "name": SUBREDDIT}
+    nodes.append(node)
+    
+    # Links
+    for i, subreddit_name in enumerate(sidebars):
+        if subreddit_name != SUBREDDIT:
+            link = {"source": query_subreddit_id, "target":i}
+            links.append(link)
+
+
+    return jsonify({'subreddits': list(sidebars), 'nodes': list(nodes), 'links': list(links)})
 
 if __name__ == '__main__':
     token_handler = Reddit_access()
