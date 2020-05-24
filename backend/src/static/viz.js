da2976bff8e85da18d15fbb5426d067b99cdf507
Alex Flores Escarcega <alexfloresescarcega@Alexs-MacBook-Pro.local>
2020-05-24 13:31:18 -0500

    broken from weird array edit error

--- backend/src/static/viz.js
+++ backend/src/static/viz.js
@@ -8,10 +8,22 @@ let url = url_prefix + "movies";
 
 var force = d3.layout.force()
     .size([width, height])
-    .nodes([{}]) // init with a single node
-    .linkDistance(30)
-    .charge(-600)
+    .nodes([]) // init empty
+    .linkDistance(200)
+    .charge(-1000)
     .on("tick", tick);
+
+var simulation = d3.forceSimulation()
+    .force("link", d3.forceLink().id(function(d) { return d.id; }))
+    .force("charge", d3.forceManyBody())
+    .force("center", d3.forceCenter(width / 2, height / 2));
+
+simulation
+    .nodes(graph.nodes)
+    .on("tick", ticked);
+
+simulation.force("link")
+    .links(graph.links);
 
 var svg = d3.select("body").append("svg")
     .attr("width", width)
@@ -25,6 +37,8 @@ var nodes = force.nodes(),
     node = svg.selectAll(".node"),
     link = svg.selectAll(".link");
 
+let nodeNameToID = {};
+let unique_id = 0;
 
 // var cursor = svg.append("circle")
 //     .attr("r", 30)
@@ -37,6 +51,7 @@ restart();
     
 
 function tick() {
+    link = svg.selectAll(".link");
     link.attr("x1", function(d) { return d.source.x; })
         .attr("y1", function(d) { return d.source.y; })
         .attr("x2", function(d) { return d.target.x; })
@@ -51,19 +66,39 @@ function tick() {
 function restart(_url = url) {
     console.log("This is my url" + _url);
     console.log(nodes);
+    console.log("links sane check " + links);
     
     d3.json(_url, (d) => {
+        console.log("This is nodes so far before adding their names and ids " + nodes)
+        console.log("This is what d.nodes looks like " + d.nodes.length);
+        var origin_name =  d.nodes[d.nodes.length - 1].name;
+        console.log("This is the origin: " + origin_name);
+        nodeNameToID[origin_name] = d.nodes.length - 1;
         d.nodes.forEach((thisNode) => {
-            console.log(thisNode.id + ": " + thisNode.name);
-            if (nodes[thisNode.id + 1] !== undefined) {
-                console.log("This id exists: ".concat(thisNode.id + 1));
-                nodes[thisNode.id + 1]["id"] = thisNode.id + 1;
-                nodes[thisNode.id + 1]["name"] = thisNode.name;
-            } else if (nodes[thisNode.id + 1] === undefined) {
-                console.log("This id didn't exist: ".concat(thisNode.id + 1));
-                nodes.push({"index" : thisNode.id + 1, "id" : thisNode.id + 1, "name": thisNode.name});
+            console.log("\n\n"+thisNode.id + ": " + thisNode.name);
+            if (thisNode.name in nodeNameToID) {
+                console.log("This node exists: " + thisNode.name + ": " + nodeNameToID[thisNode.name]);
+            } else {
+                console.log("This node didn't exist: " + thisNode.name);
+                nodes.push({"name": thisNode.name});
+                nodeNameToID[thisNode.name] = unique_id;
+                console.log(thisNode.name + " has unique_id: " + unique_id);
+                console.log(thisNode.name + " originates from " + origin_name);
+                console.log(thisNode.name + " maps to id " + nodeNameToID[origin_name]);
+                if(thisNode.name != origin_name){
+                    console.log("Pushing to links: " + nodeNameToID[origin_name] +", " + unique_id);
+                    links.push({source: nodeNameToID[origin_name], target: unique_id});
+                    console.log(links);
+                } else {
+                    console.log("culprits: " + thisNode.name + " " + origin_name);
+                }
+                unique_id += 1;
             }
         });
+        console.log(links);
+        links[0].source = nodeNameToID[origin_name];
+        links[0].target = 0;
+        console.log(links);
         var node = svg.selectAll(".node");
         var nodeEnter = node.data(nodes, function(d, i) { return d.name; }).enter()
             .append("g")
@@ -80,6 +115,9 @@ function restart(_url = url) {
                 .attr("dx", 0)
                 .attr("dy", ".35em")
                 .text(function(d) { return d.name });
+
+        var link = svg.selectAll(".link");
+
 
         console.log("The size of the selection is: " + svg.selectAll('.node').data(nodes).size());
         
